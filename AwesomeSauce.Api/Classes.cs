fd91ac50272ada243a532c2235b578719165e656
krzysztof <krzysztof.mohr@live.com>
2018-04-29 13:53:52 +0100

    added project scaffolding

---
+++ AwesomeSauce.Api/Classes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeSauce.Api
+{
+    public interface IComponent
+    {
+        string Name { get; }
+    }
+    public class ComponentA
+    {
+        private readonly IComponent _componentB;
+        public ComponentA(IComponent componentB)
+        {
+            this._componentB = componentB;
+        }
+    }
+    public class ComponentB : IComponent
+    {
+        public string Name { get; set; } = nameof(ComponentB);
+    }
+}
---
+++ AwesomeSauce.Api/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AwesomeSauce.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration(config => config.AddJsonFile("appSettings.json"))
+                .ConfigureLogging(logging => logging.AddConsole().AddDebug())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+        }
+            //WebHost.CreateDefaultBuilder(args)
+            //    .UseStartup<Startup>()
+            //    .Build();
+    }
+}
