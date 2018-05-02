ac4b6618571f46107fe3f9cd5835ff3f0e6a9aa8
krzysztof <krzysztof.mohr@live.com>
2018-05-03 00:05:44 +0100

    added async middleware steps and default step

--- AwesomeSauce.Api/Startup.cs
+++ AwesomeSauce.Api/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,18 +24,47 @@ namespace AwesomeSauce.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IComponent, ComponentB>();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IComponent component)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //
 
-            app.UseMvc();
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/foo")
+                {
+                    await context.Response.WriteAsync($"Welcome Foo");
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/bar")
+                {
+                    await context.Response.WriteAsync($"Welcome Bar");
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
+            app.Run(async context => await context.Response.WriteAsync($"Welcome to default"));
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //app.UseMvc();
         }
     }
 }
